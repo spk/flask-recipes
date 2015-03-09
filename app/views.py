05891b5a2dfb6d78f849ce1ade76be772e9f70e4
Laurent Arnoud <laurent@spkdev.net>
2015-03-09 21:11:12 +0100

    Improve pagination and fix it for categories

--- app/views.py
+++ app/views.py
@@ -19,23 +19,24 @@ def show(id):
     recipe = Recipe.query.get_or_404(id)
     return render_template('show.html', recipe=recipe)
 
-@app.route('/categories/<title>')
-def categories(title):
-    page, per_page = get_page_items()
+@app.route('/categories/<title>', defaults={'page': 1})
+@app.route('/categories/<title>/page/<int:page>')
+def categories(title, page):
+    per_page = get_per_page()
     pagination = Category.query.filter_by(title=title).first_or_404().recipes.paginate(page, per_page)
-    return render_template('index.html', pagination=pagination)
+    return render_template('index.html', pagination=pagination, title=title)
 
-@app.route('/')
-def index():
-    page, per_page = get_page_items()
+@app.route('/', defaults={'page': 1})
+@app.route('/page/<int:page>')
+def index(page):
+    per_page = get_per_page()
     pagination = Recipe.query.order_by(Recipe.created_at.desc()).paginate(page, per_page)
     return render_template('index.html', pagination=pagination)
 
-def get_page_items():
-    page = int(request.args.get('page', 1))
+def get_per_page():
     per_page = request.args.get('per_page')
     if not per_page:
         per_page = app.config.get('PER_PAGE', 10)
     else:
         per_page = int(per_page)
-    return page, per_page
+    return per_page
