47e919455b55b2c9a0d091da3cf00a381af6d2ad
Laurent Arnoud <laurent@spkdev.net>
2015-03-13 20:50:06 +0100

    Rename table name

--- app/models.py
+++ app/models.py
@@ -2,12 +2,13 @@ from app import db
 from datetime import datetime
 from sqlalchemy.sql.expression import func
 
-categories = db.Table('categories',
-        db.Column('category_id', db.Integer, db.ForeignKey('category.id')),
-        db.Column('recipe_id', db.Integer, db.ForeignKey('recipe.id'))
+categories = db.Table('recipes_categories',
+        db.Column('category_id', db.Integer, db.ForeignKey('categories.id')),
+        db.Column('recipe_id', db.Integer, db.ForeignKey('recipes.id'))
         )
 
 class Recipe(db.Model):
+    __tablename__ = 'recipes'
     """
     Model for the Recipe created by the users.
     """
@@ -30,17 +31,20 @@ class Recipe(db.Model):
         return self.query.order_by(func.random()).limit(1)
 
 class Direction(db.Model):
+    __tablename__ = 'directions'
     id = db.Column(db.Integer, primary_key=True)
     step = db.Column(db.Text)
-    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'))
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipes.id'))
 
 class Ingredient(db.Model):
+    __tablename__ = 'ingredients'
     id = db.Column(db.Integer, primary_key=True)
     quantity = db.Column(db.Integer)
     unit = db.Column(db.String(30))
     item = db.Column(db.String(30), index=True)
-    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'))
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipes.id'))
 
 class Category(db.Model):
+    __tablename__ = 'categories'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(30), index=True, unique=True)
