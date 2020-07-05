fe8a0c6fa1305741eee6afd5c28deee5d05d3d9a
Laurent Arnoud <laurent@spkdev.net>
2020-07-05 12:33:23 +0200

    Update to gunicorn 20.0.4

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from app import create_app
 
+App = create_app()
+
 if __name__ == "__main__":
-    app = create_app()
-    print(app.url_map)
-    app.run(host='0.0.0.0')
+    App.run()
