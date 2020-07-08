5c1c2e57e2af2636b647cf4e932e7c1fa2e354b4
Laurent Arnoud <laurent@spkdev.net>
2020-07-08 11:23:15 +0200

    Improve db_manage.py script add create/drop

--- db_manage.py
+++ db_manage.py
@@ -1,5 +1,5 @@
 import sys
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, inspect
 from app import create_app
 from app.extensions import db
 from app.config import config, SELECTED_CONFIG
@@ -9,30 +9,46 @@ class DBManage(object):
     def __init__(self):
         self.url = config[SELECTED_CONFIG].POSTGRES_URL
         self.engine = create_engine(self.url, echo=True)
-        self.conn = self.engine.connect()
 
     def up(self, db_name=config[SELECTED_CONFIG].POSTGRES_DB):
-        self.conn.execute("commit")
-        self.conn.execute("create database {0}".format(db_name))
-        self.conn.close()
+        connection = self.engine.connect()
+        connection.execute("COMMIT")
+        connection.execute("CREATE DATABASE {0}".format(db_name))
+        connection.close()
+        self.create()
+
+    def down(self, db_name=config[SELECTED_CONFIG].POSTGRES_DB):
+        connection = self.engine.connect()
+        connection.execute("COMMIT")
+        connection.execute("DROP DATABASE {0}".format(db_name))
+        connection.close()
+
+    def create(self):
         app = create_app()
         with app.app_context():
             db.create_all()
 
-    def down(self, db_name=config[SELECTED_CONFIG].POSTGRES_DB):
-        self.conn.execute("commit")
-        self.conn.execute("drop database {0}".format(db_name))
-        self.conn.close()
+    def drop(self):
+        engine = create_engine(
+            config[SELECTED_CONFIG].SQLALCHEMY_DATABASE_URI,
+            echo=True)
+        connection = engine.connect()
+        inspector = inspect(engine)
+        for table_name in inspector.get_table_names():
+            connection.execute("DROP TABLE {0} CASCADE".format(table_name))
+        connection.close()
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print('USAGE: {0} [up|down]'.format(sys.argv[0]))
-        sys.exit(1)
+    options = {
+        "up": DBManage().up,
+        "down": DBManage().down,
+        "create": DBManage().create,
+        "drop": DBManage().drop
+    }
+    if sys.argv[1] in options:
+        options[sys.argv[1]]()
+        sys.exit(0)
     else:
-        if sys.argv[1] == "up":
-            DBManage().up()
-            sys.exit(0)
-        elif sys.argv[1] == "down":
-            DBManage().down()
-            sys.exit(0)
+        print('USAGE: {0} [{1}]'.format(sys.argv[0], "|".join(options.keys())))
+        sys.exit(1)
